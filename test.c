f0dcd3d08166374204759b068e60926b4af20ac2
alindapal07 <151637518+alindapal07@users.noreply.github.com>
2023-12-04 14:12:04 +0530

    start of c folder

---
+++ test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+  
+int main()
+   {                
+   	
+   	
+   	
+   	
+   	
+   	return 0;
+   }
